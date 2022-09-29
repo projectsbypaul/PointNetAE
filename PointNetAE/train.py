82898426d1eb656f1f8a345358479aa1636c0712
Paul Schuster <paul.schuster.dresden@web.de>
2022-09-29 17:12:16 +0200

    added custom training loop

--- PointNetAE/train.py
+++ PointNetAE/train.py
@@ -2,7 +2,6 @@ import os
 import glob
 import pickle
 import h5py
-import tensorflow
 import trimesh
 import numpy as np
 import tensorflow as tf
@@ -14,10 +13,17 @@ from matplotlib import pyplot as plt
 from model import build_model
 from tensorflow_graphics.nn.loss.chamfer_distance import evaluate
 from keras.callbacks import LambdaCallback
+import time
 
 
-def loss_func(y_true, y_pred):
+def loss_func(y_true, y_pred, batch_size):
+    y_true = tf.cast(y_true, dtype='double')
+    y_pred = tf.cast(y_pred, dtype='double')
+
     loss = evaluate(y_true, y_pred)
+
+    loss = tf.reduce_mean(loss)
+
     return loss
 
 
@@ -30,40 +36,67 @@ LEARNING_RATE = 0.001
 DATASET = ['ModelNet10', ]
 ARCHITECTURE = 'PointNet_AE'
 
-# checkpoint callback
-checkpoint_path = r"versions/cp-{epoch:04d}.ckpt"
-checkpoint_dir = os.path.dirname(checkpoint_path)
-
-cp_callback = tf.keras.callbacks.ModelCheckpoint(
-    checkpoint_path, verbose=1, save_weights_only=True,
-    # Save weights, every epoch.
-    period=1)
-
-# json log callback
-json_log = open('versions/loss_log.json', mode='wt', buffering=1)
-json_logging_callback = LambdaCallback(
-            on_epoch_end=lambda epoch, logs: json_log.write(
-                json.dumps({'epoch': epoch, 'loss': logs['loss'], 'val_loss':logs['val_loss']}) + '\n'),
-            on_train_end=lambda logs: json_log.close()
-)
-
-
-# compile and train
-
-my_model = build_model(NUM_POINTS, BATCH_SIZE, NUM_CLASSES)
-
-my_model.compile(
-    loss=loss_func,
-    optimizer=keras.optimizers.Adam(learning_rate=LEARNING_RATE))
+# dataset
 
 with open(r'inputs\train_points.pkl', 'rb') as f:
-    train = pickle.load(f)
+    train_points = pickle.load(f)
 
 with open(r'inputs\test_points.pkl', 'rb') as f:
-    test = pickle.load(f)
+    test_points = pickle.load(f)
 
-my_model.save_weights(checkpoint_path.format(epoch=0))
+train_dataset = tf.data.Dataset.from_tensor_slices((train_points, train_points))
+train_dataset = train_dataset.shuffle(100).batch(BATCH_SIZE)
 
-my_model.fit(train, train, batch_size=BATCH_SIZE, epochs=EPOCHS, shuffle=True, validation_data=(test, test),
-             callbacks=[cp_callback, json_logging_callback])
+test_dataset = tf.data.Dataset.from_tensor_slices((test_points, test_points))
 
+# model and optimizer
+
+model = build_model(NUM_POINTS, BATCH_SIZE, NUM_CLASSES)
+
+optimizer = keras.optimizers.Adam(learning_rate=LEARNING_RATE)
+
+# run training
+
+for epoch in range(EPOCHS):
+    print("\nStart of epoch %d" % (epoch,))
+
+    start_time = time.time()
+
+    # Iterate over the batches of the dataset.
+    for step, (x_batch_train, y_batch_train) in enumerate(train_dataset):
+
+        # Open a GradientTape to record the operations run
+        # during the forward pass, which enables auto-differentiation.
+        with tf.GradientTape() as tape:
+
+            # Run the forward pass of the layer.
+            # The operations that the layer applies
+            # to its inputs are going to be recorded
+            # on the GradientTape.
+            logits = model(x_batch_train, training=True)  # Logits for this minibatch
+
+            # Compute the loss value for this minibatch.
+            loss_value = loss_func(y_batch_train, logits, BATCH_SIZE)
+
+        # Use the gradient tape to automatically retrieve
+        # the gradients of the trainable variables with respect to the loss.
+        grads = tape.gradient(loss_value, model.trainable_weights)
+        # Run one step of gradient descent by updating
+        # the value of the variables to minimize the loss.
+        optimizer.apply_gradients(zip(grads, model.trainable_weights))
+
+        # Log every 200 batches.
+        if step % 1 == 0:
+            print(
+                "Training loss (for one batch) at step %d: %.4f"
+                % (step, float(loss_value))
+            )
+            print("Seen so far: %s samples" % ((step + 1) * BATCH_SIZE))
+
+    # Validation loss
+
+    for (x_batch_test, y_batch_test) in test_dataset:
+        val_loss_value = loss_func(x_batch_test, y_batch_test, BATCH_SIZE)
+
+    print("Validation Loss: %.4f" % (float(val_loss_value)))
+    print("Time taken: %.2fs" % (time.time() - start_time))
